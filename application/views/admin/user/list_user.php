72153afddb3de7dfae88f5e8b1f931261687e200
Sanjay Rabidas <rabidassanjay@gmail.com>
2016-01-09 18:00:33 +0530

    Change : Styles

--- application/views/admin/user/list_user.php
+++ application/views/admin/user/list_user.php
@@ -1,1 +1,25 @@
-Hello
+<div class="f8-sec-inner-block">
+  <table class="table table-default">
+    <tr>
+      <td></td><td></td><td></td><td></td><td></td><td></td>
+    </tr>
+    <tr>
+      <td></td><td></td><td></td><td></td><td></td><td></td>
+    </tr>
+    <tr>
+      <td></td><td></td><td></td><td></td><td></td><td></td>
+    </tr>
+    <tr>
+      <td></td><td></td><td></td><td></td><td></td><td></td>
+    </tr>
+    <tr>
+      <td></td><td></td><td></td><td></td><td></td><td></td>
+    </tr>
+    <tr>
+      <td></td><td></td><td></td><td></td><td></td><td></td>
+    </tr>
+    <tr>
+      <td></td><td></td><td></td><td></td><td></td><td></td>
+    </tr>
+  </table>
+</div>
