7e2ba26631b2536b1673ad78725a33604afcc682
Sanjay Rabidas <rabidassanjay@gmail.com>
2016-01-09 14:22:02 +0530

    change: table name in User_model

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -15,7 +15,7 @@ class User_model extends CI_Model {
   }
 
   public function get_all_users(){
-          $query = $this->db->get('entries');
+          $query = $this->db->get('users');
           return $query->result();
   }
 
