df3c2e2d01949e7b173d7e6b8ccaba1589d6455d
Sanjay Rabidas <rabidassanjay@gmail.com>
2016-01-26 23:57:57 +0530

    Added RBAC for admin pages

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -11,57 +11,84 @@ class Admin extends CI_Controller {
 		$this->load->helper('url_helper');
 	}
 
+	private function authorized() {
+		return $this->ion_auth->logged_in() && $this->ion_auth->is_admin();
+	}
+
 	public function index()	{
-		$this->load->view('admin/home');
+		if($this->authorized()){
+			$this->load->view('admin/home');
+		}
+		else{
+			redirect(base_url('auth/login'), 'refresh');
+		}
 	}
 
 	public function users($action)	{
-		$data['sub_view'] = "";
-		if($action === "list"){
-			$data['users'] = $this->user_model->get_all_users();
-			$data['roles'] = $this->role_model->get_all_roles();
+		if($this->authorized()){
+			$data['sub_view'] = "";
+			if($action === "list"){
+				$data['users'] = $this->user_model->get_all_users();
+				$data['roles'] = $this->role_model->get_all_roles();
 
-			$data['sub_view'] = "admin/user/list_user";
+				$data['sub_view'] = "admin/user/list_user";
+			}
+			else if($action === 'add'){
+				$data['sub_view'] = "admin/user/add_user";
+			}
+			else if($action === 'update') {
+				$data['sub_view'] = "admin/user/update_user";
+			}
+			$this->load->view('admin/home', $data);
 		}
-		else if($action === 'add'){
-			$data['sub_view'] = "admin/user/add_user";
+		else{
+			redirect(base_url('auth/login'), 'refresh');
 		}
-		else if($action === 'update') {
-			$data['sub_view'] = "admin/user/update_user";
-		}
-		$this->load->view('admin/home', $data);
+
 	}
 
 	public function roles($action)	{
-		$data['sub_view'] = "";
-		if($action === "list"){
-			$data['roles'] = $this->role_model->get_all_roles();
-			$data['sub_view'] = "admin/role/list_role";
+		if($this->authorized()){
+			$data['sub_view'] = "";
+			if($action === "list"){
+				$data['roles'] = $this->role_model->get_all_roles();
+				$data['sub_view'] = "admin/role/list_role";
+			}
+			else if($action === 'add'){
+				$data['sub_view'] = "admin/role/add_role";
+			}
+			else if($action === 'update') {
+				$data['sub_view'] = "admin/role/update_role";
+			}
+			$this->load->view('admin/home', $data);
 		}
-		else if($action === 'add'){
-			$data['sub_view'] = "admin/role/add_role";
+		else{
+			redirect(base_url('auth/login'), 'refresh');
 		}
-		else if($action === 'update') {
-			$data['sub_view'] = "admin/role/update_role";
-		}
-		$this->load->view('admin/home', $data);
+
 
 	}
 
 	public function posts($action)	{
-		$data['sub_view'] = "";
-		if($action === "list"){
-			$data['users'] = $this->user_model->get_all_users();
-			$data['posts'] = $this->post_model->get_all_posts();
-			$data['sub_view'] = "admin/post/list_post";
+		if($this->authorized()){
+			$data['sub_view'] = "";
+			if($action === "list"){
+				$data['users'] = $this->user_model->get_all_users();
+				$data['posts'] = $this->post_model->get_all_posts();
+				$data['sub_view'] = "admin/post/list_post";
+			}
+			else if($action === 'add'){
+				$data['sub_view'] = "admin/post/add_post";
+			}
+			else if($action === 'update') {
+				$data['sub_view'] = "admin/post/update_post";
+			}
+			$this->load->view('admin/home', $data);
 		}
-		else if($action === 'add'){
-			$data['sub_view'] = "admin/post/add_post";
+		else{
+			redirect(base_url('auth/login'), 'refresh');
 		}
-		else if($action === 'update') {
-			$data['sub_view'] = "admin/post/update_post";
-		}
-		$this->load->view('admin/home', $data);
+
 
 	}
 }
