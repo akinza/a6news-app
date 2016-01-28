025ad7b35463883a6f0843233e6769c42a00c16c
Sanjay Rabidas <rabidassanjay@gmail.com>
2016-01-28 19:41:35 +0530

    Change CSS

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -30,7 +30,7 @@
             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
           </div>
           <button type="submit" class="btn btn-lg btn-primary btn-block">Sign In ...</button>
-          <!-- <hr> -->
+          <hr>
           <div class="form-group">
             <p>Already registered? <a class="btn btn-link" href="<?php echo base_url("auth/login"); ?>">Login</a></p>
           </div>
