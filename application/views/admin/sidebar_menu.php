3cf3dd169eaab2764c2d5061ce7ddc75483737a1
Sanjay Rabidas <rabidassanjay@gmail.com>
2016-02-05 12:22:21 +0530

    Fix Sidebar menus

--- application/views/admin/sidebar_menu.php
+++ application/views/admin/sidebar_menu.php
@@ -1,8 +1,9 @@
 <div class="f8-sec-admin-menus">
   <div class="list-group f8-menu">
-    <a class="list-group-item" href="<?php echo base_url('admin/posts/add');?>">Add Post</a>
+    <a class="list-group-item active" href="<?php echo base_url('admin/posts/add');?>">Add Post</a>
     <a class="list-group-item" href="<?php echo base_url('admin/posts/list');?>">Show All Posts</a>
-
+  </div>
+  <div class="list-group f8-menu">
     <a class="list-group-item" href="<?php echo base_url('auth');?>">Show Users</a>
     <a class="list-group-item" href="<?php echo base_url('auth/create_user');?>">Add New User</a>
     <a class="list-group-item" href="<?php echo base_url('auth/create_group');?>">Create New User Group</a>
