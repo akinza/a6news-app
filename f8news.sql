e4bcedf92752c5ec07dfc3fc367c671112db9459
Sanjay Rabidas <rabidassanjay@gmail.com>
2016-02-08 17:19:47 +0530

    Updated Database

--- f8news.sql
+++ f8news.sql
@@ -16,37 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `f8users`
---
-
-DROP TABLE IF EXISTS `f8users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `f8users` (
-  `uid` bigint(20) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(30) NOT NULL,
-  `last_name` varchar(30) NOT NULL,
-  `email_id` varchar(50) NOT NULL,
-  `secret` varchar(100) NOT NULL,
-  `role_id` int(11) NOT NULL,
-  PRIMARY KEY (`uid`),
-  UNIQUE KEY `email_id` (`email_id`),
-  KEY `role_id` (`role_id`),
-  CONSTRAINT `f8users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `f8users`
---
-
-LOCK TABLES `f8users` WRITE;
-/*!40000 ALTER TABLE `f8users` DISABLE KEYS */;
-INSERT INTO `f8users` VALUES (1,'Sanjay','Rabidas','rabidassanjay@gmail.com','*01C91AD24A21E916A3D46F5DAE633733DF34A2F9',1);
-/*!40000 ALTER TABLE `f8users` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `groups`
 --
 
@@ -58,7 +27,7 @@ CREATE TABLE `groups` (
   `name` varchar(20) NOT NULL,
   `description` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +36,7 @@ CREATE TABLE `groups` (
 
 LOCK TABLES `groups` WRITE;
 /*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-INSERT INTO `groups` VALUES (1,'admin','Administrator'),(2,'members','General User');
+INSERT INTO `groups` VALUES (1,'admin','System Administrator'),(2,'members','General User'),(3,'Editor','News Editors');
 /*!40000 ALTER TABLE `groups` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -97,57 +66,35 @@ LOCK TABLES `login_attempts` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `posts`
+-- Table structure for table `news`
 --
 
-DROP TABLE IF EXISTS `posts`;
+DROP TABLE IF EXISTS `news`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `posts` (
+CREATE TABLE `news` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) NOT NULL,
-  `body` text NOT NULL,
+  `slug` varchar(128) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `news_short` varchar(500) NOT NULL,
+  `news_full` text NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `modified` timestamp NULL DEFAULT NULL,
   `author` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `author` (`author`),
-  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`author`) REFERENCES `f8users` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  UNIQUE KEY `slug` (`slug`),
+  KEY `author` (`author`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `posts`
+-- Dumping data for table `news`
 --
 
-LOCK TABLES `posts` WRITE;
-/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `roles`
---
-
-DROP TABLE IF EXISTS `roles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `roles` (
-  `role_id` int(11) NOT NULL AUTO_INCREMENT,
-  `role_name` varchar(30) NOT NULL,
-  PRIMARY KEY (`role_id`),
-  UNIQUE KEY `role_name` (`role_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `roles`
---
-
-LOCK TABLES `roles` WRITE;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'ADMIN'),(3,'AUTHOR'),(2,'EDITOR');
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+LOCK TABLES `news` WRITE;
+/*!40000 ALTER TABLE `news` DISABLE KEYS */;
+INSERT INTO `news` VALUES (1,'Venmo went down after the Super Bowl, and some people think they know why','Venmo went down after the Super Bowl, and some people think they know why','<p>So you thought the dabbing quarterback with the million-dollar smile,&nbsp;<a href=\"http://mashable.com/2016/02/03/cam-newton-sunshine/\">Cam Newton,</a>&nbsp;was going to win, huh? Well now, if you put your money where your mouth was &mdash;&nbsp;that is, you made a bet on the game with your friends &mdash;&nbsp;it may be time to pay up.&nbsp;</p>\r\n','<p>So you thought the dabbing quarterback with the million-dollar smile,&nbsp;<a href=\"http://mashable.com/2016/02/03/cam-newton-sunshine/\">Cam Newton,</a>&nbsp;was going to win, huh? Well now, if you put your money where your mouth was &mdash;&nbsp;that is, you made a bet on the game with your friends &mdash;&nbsp;it may be time to pay up.&nbsp;</p>\r\n\r\n<p>But if you decided to do that via the payment app&nbsp;<a href=\"http://mashable.com/category/venmo/\">Venmo</a>, you were, at least temporarily, flat out of luck.</p>\r\n\r\n<p>Just minutes after&nbsp;<a href=\"http://mashable.com/category/super-bowl-50/\">Super Bowl 50</a>&nbsp;ended, the mobile payments service went down, or at least lagged, for many users. A number of them switched over to Twitter to voice their complaints &mdash;&nbsp;and their conspiracy theories.</p>\r\n\r\n<p><strong>SEE ALSO:&nbsp;<a href=\"http://mashable.com/2016/01/28/venmo-in-app-purchases/\">You can now make in-app purchases with Venmo</a></strong></p>\r\n\r\n<p>What theories? Well, some people (who have offered no proof whatsoever) think the down time has to do with bets being paid off now that the favorites to win the Super Bowl, the Carolina Panthers, have lost. That would presumably result in a ton of payment activity from all the people who were so confident about a Panthers win.&nbsp;</p>\r\n\r\n<p>Of course, Venmo didn&#39;t attribute the issue to any betting activity, nor did the company offer&nbsp;<em>any</em>explanation for the service interruption.</p>\r\n\r\n<p>But that didn&#39;t stop Venmo users on Twitter from speculating about the timing.</p>\r\n\r\n<p>We may never know what was behind the app&#39;s problems so soon after the Super Bowl.&nbsp;But if people were indeed using it to pay off bets, this wasn&#39;t a great advertisement for the service. (Or maybe it was.)</p>\r\n\r\n<p>Venmo did not immediately respond to a request for comment.</p>\r\n\r\n<p><em>Have something to add to this story? Share it in the comments.</em></p>\r\n','0000-00-00 00:00:00',NULL,1);
+/*!40000 ALTER TABLE `news` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -176,7 +123,7 @@ CREATE TABLE `users` (
   `company` varchar(100) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -185,7 +132,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'127.0.0.1','administrator','$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36','','admin@admin.com','',NULL,NULL,NULL,1268889823,1454653840,1,'Admin','istrator','ADMIN','0');
+INSERT INTO `users` VALUES (1,'127.0.0.1','administrator','$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36','','admin@admin.com','',NULL,NULL,NULL,1268889823,1454931647,1,'Admin','istrator','ADMIN','0'),(2,'::1',NULL,'$2y$08$7kjXO3aQj8iZnrr7XPnXb.br7BNK8tTxaAJhOvMtLh2p2NM2MNwlK',NULL,'rabidassanjay@gmail.com',NULL,NULL,NULL,NULL,1454910020,NULL,1,'Sanjay','Rabidas',NULL,'9545142899');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -204,9 +151,9 @@ CREATE TABLE `users_groups` (
   UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
   KEY `fk_users_groups_users1_idx` (`user_id`),
   KEY `fk_users_groups_groups1_idx` (`group_id`),
-  CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -215,7 +162,7 @@ CREATE TABLE `users_groups` (
 
 LOCK TABLES `users_groups` WRITE;
 /*!40000 ALTER TABLE `users_groups` DISABLE KEYS */;
-INSERT INTO `users_groups` VALUES (1,1,1),(2,1,2);
+INSERT INTO `users_groups` VALUES (1,1,1),(2,1,2),(7,2,1),(8,2,2),(9,2,3);
 /*!40000 ALTER TABLE `users_groups` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -228,4 +175,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-02-05 12:17:16
+-- Dump completed on 2016-02-08 17:19:23
