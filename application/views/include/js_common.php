72543bfd42e1142eb088111c73654f8711fc16d9
Sanjay Rabidas <rabidassanjay@gmail.com>
2016-02-12 19:35:23 +0530

    Fix dropdown menu

--- application/views/include/js_common.php
+++ application/views/include/js_common.php
@@ -1,4 +1,5 @@
 <script src="<?php echo base_url( COMPONENTS."/jquery/dist/jquery.js");?>"></script>
 <script src="<?php echo base_url( COMPONENTS."/underscore/underscore.js");?>"></script>
-<script src="<?php echo base_url( JS."/bootstrap.min.js" );?>"></script>
+<script src="<?php echo base_url( COMPONENTS."/bootstrap/dist/js/bootstrap.min.js" );?>"></script>
+<script src="<?php echo base_url( MODULE."/ckeditor/ckeditor.js");?>"></script>
 <script src="<?php echo base_url( JS."/main.js" );?>"></script>
