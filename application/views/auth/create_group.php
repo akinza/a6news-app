9a2511015d5dbd1d18fd8c9e2a392fceab390bba
Sanjay Rabidas <rabidassanjay@gmail.com>
2016-02-16 02:00:36 +0530

    Change : layout of auth views

--- application/views/auth/create_group.php
+++ application/views/auth/create_group.php
@@ -7,11 +7,11 @@
   <body class="">
     <?php $this->load->view('include/header'); ?>
       <div class="f8-admin-container container">
-        <section class="f8-sec-admin-sidebar  col-lg-2 col-md-3 col-sm-4 col-xs-12">
+        <section class="f8-sec-admin-sidebar  col-lg-3 col-md-3 col-sm-4 col-xs-12">
           <?php $this->load->view('admin/sidebar_menu'); ?>
         </section>
-        <section class="f8-sec-admin-body  col-lg-10 col-md-9 col-sm-8 col-xs-12">
-          <div class="f8-sec-inner-block  col-lg-6 col-md-6 col-sm-8 col-xs-12">
+        <section class="f8-sec-admin-body  col-lg-9 col-md-9 col-sm-8 col-xs-12">
+          <div class="f8-sec-inner-block">
             <div class="f8-sec-heading">
               <?php echo lang('create_group_heading');?>
             </div>
@@ -19,21 +19,21 @@
             <div id="infoMessage"><?php echo $message;?></div>
             <?php echo form_open("auth/create_group");?>
               <div class="form-group">
-                <label for="group_name" class="col-sm-3 control-label" style="text-align:left !important;">Group Name</label>
-                <div class="col-sm-8">
+                <label for="group_name" class="col-sm-3 control-label" >Group Name</label>
+                <div class="col-sm-6">
                   <input name="group_name" type="text" class="form-control" id="group_name" placeholder="Group Name">
                 </div>
               </div>
               <div class="form-group">
-                <label for="input-last-name" class="col-sm-3 control-label"  style="text-align:left !important;">Description</label>
-                <div class="col-sm-8">
+                <label for="input-last-name" class="col-sm-3 control-label">Description</label>
+                <div class="col-sm-6">
                   <input name="description" type="text" class="form-control" id="description" placeholder="Description">
                 </div>
               </div>
-
+              <br>
               <div class="form-group">
-                <div class="col-sm-11">
-                  <button type="submit" class="btn btn-warning btn-block">Create Group</button>
+                <div class="col-sm-9 text-right">
+                  <button type="submit" class="btn btn-warning">Create Group</button>
                 </div>
               </div>
               <?php echo form_close();?>
--- application/views/auth/create_user.php
+++ application/views/auth/create_user.php
@@ -8,56 +8,56 @@
     <?php $this->load->view('include/header'); ?>
 
       <div class="f8-admin-container container">
-        <section class="f8-sec-admin-sidebar  col-lg-2 col-md-3 col-sm-4 col-xs-12">
+        <section class="f8-sec-admin-sidebar  col-lg-3 col-md-3 col-sm-3 col-xs-12">
           <?php $this->load->view('admin/sidebar_menu'); ?>
         </section>
-        <section class="f8-sec-admin-body  col-lg-10 col-md-9 col-sm-8 col-xs-12">
-          <div class="f8-sec-inner-block  col-lg-8 col-md-8 col-sm-8 col-xs-12">
+        <section class="f8-sec-admin-body  col-lg-9 col-md-9 col-sm-6 col-xs-12">
+          <div class="f8-sec-inner-block">
             <div class="f8-sec-heading">
               Add New Users
             </div>
-            <p><?php echo lang('create_user_subheading');?></p>
+            <!-- <p><?php echo lang('create_user_subheading');?></p> -->
             <div id="infoMessage"><?php echo $message;?></div>
             <?php echo form_open("auth/create_user");?>
               <div class="form-group">
-                <label for="input-first-name" class="col-sm-4 control-label">First Name</label>
-                <div class="col-sm-8">
+                <label for="input-first-name" class="col-sm-3 control-label">First Name</label>
+                <div class="col-sm-6">
                   <input name="first_name" type="text" class="form-control" id="input-first-name" placeholder="First Name">
                 </div>
               </div>
               <div class="form-group">
-                <label for="input-last-name" class="col-sm-4 control-label">Last Name</label>
-                <div class="col-sm-8">
+                <label for="input-last-name" class="col-sm-3 control-label">Last Name</label>
+                <div class="col-sm-6">
                   <input name="last_name" type="text" class="form-control" id="input-last-name" placeholder="Last Name">
                 </div>
               </div>
               <div class="form-group">
-                <label for="input-email" class="col-sm-4 control-label">Email</label>
-                <div class="col-sm-8">
+                <label for="input-email" class="col-sm-3 control-label">Email</label>
+                <div class="col-sm-6">
                   <input name="email" type="eamil" class="form-control" id="input-email" placeholder="Email">
                 </div>
               </div>
               <div class="form-group">
-                <label for="input-phone" class="col-sm-4 control-label">Phone/ Mobile</label>
-                <div class="col-sm-8">
+                <label for="input-phone" class="col-sm-3 control-label">Phone/ Mobile</label>
+                <div class="col-sm-6">
                   <input name="phone" type="text" class="form-control" id="input-phone" placeholder="Phone">
                 </div>
               </div>
               <div class="form-group">
-                <label for="input-secret" class="col-sm-4 control-label">Password</label>
-                <div class="col-sm-8">
+                <label for="input-secret" class="col-sm-3 control-label">Password</label>
+                <div class="col-sm-6">
                   <input name="password" type="password" class="form-control" id="input-secret" placeholder="Password">
                 </div>
               </div>
               <div class="form-group">
-                <label for="input-secret" class="col-sm-4 control-label">Confirm Password</label>
-                <div class="col-sm-8">
+                <label for="input-secret" class="col-sm-3 control-label">Confirm Password</label>
+                <div class="col-sm-6">
                   <input name="password_confirm" type="password" class="form-control" id="input-secret" placeholder="Confirm Password">
                 </div>
               </div>
               <!-- <div class="form-group">
-                <label for="select-role" class="col-sm-4 control-label">Role</label>
-                <div class="col-sm-8">
+                <label for="select-role" class="col-sm-3 control-label">Role</label>
+                <div class="col-sm-6">
                   <select name="role_id" class="form-control">
                     <option value="-1">Select User Role</option>
                     <option value="1">Beta</option>
@@ -65,10 +65,10 @@
                   </select>
                 </div>
               </div> -->
-
+              <hr>
               <div class="form-group">
-                <div class="col-sm-8 pull-right">
-                  <button type="submit" class="btn btn-primary btn-lg">Add User</button>
+                <div class="col-sm-12 text-right">
+                  <button type="submit" class="btn btn-primary btn-block btn-lg">Add User</button>
                 </div>
               </div>
               <?php echo form_close();?>
