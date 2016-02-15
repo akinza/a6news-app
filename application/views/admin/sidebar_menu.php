a44f886e7e2b46ef6efc1f7014d48ef75b017184
Sanjay Rabidas <rabidassanjay@gmail.com>
2016-02-15 20:34:03 +0530

    Added More admin menu options

--- application/views/admin/sidebar_menu.php
+++ application/views/admin/sidebar_menu.php
@@ -3,8 +3,17 @@
     <a class="list-group-item active" href="<?php echo base_url('article/create');?>">Add Post</a>
   </div>
   <div class="list-group f8-menu">
-    <a class="list-group-item" href="<?php echo base_url('article');?>">Show All Posts</a>
+    <a class="list-group-item" href="<?php echo base_url('#');?>">Create Image Gallery</a>
+    <a class="list-group-item" href="<?php echo base_url('#');?>">View All Image Galleries</a>
+  </div>
+  <div class="list-group f8-menu">
+    <a class="list-group-item" href="<?php echo base_url('#');?>">Upload Banner</a>
+    <a class="list-group-item" href="<?php echo base_url('#');?>">Manage Banners</a>
+  </div>
+  <div class="list-group f8-menu">
+    <a class="list-group-item" href="<?php echo base_url('article');?>">Manage Posts</a>
     <a class="list-group-item" href="<?php echo base_url('article/create_category');?>">Add News Category</a>
+    <a class="list-group-item" href="<?php echo base_url('article/create_category');?>">Manage Category</a>
   </div>
   <div class="list-group f8-menu">
     <a class="list-group-item" href="<?php echo base_url('auth');?>">Show Users</a>
