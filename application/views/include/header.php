e3cbb1f99ce3f66e6e133ed469144c467cfeb3c0
Sanjay Rabidas <rabidassanjay@gmail.com>
2016-03-11 08:33:56 +0530

    deactivated home link

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -15,7 +15,7 @@
     </div>
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="<?php echo base_url("/home"); ?>">Home</a></li>
+        <li><a href="<?php echo base_url("/home"); ?>">Home</a></li>
         <li><a href="<?php echo base_url("/news"); ?>">News</a></li>
         <li><a href="<?php echo base_url("/p/national"); ?>">National</a></li>
         <li><a href="<?php echo base_url("/p/business"); ?>">Business</a></li>
