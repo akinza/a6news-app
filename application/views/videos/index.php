a84fb8e3dbc1268a026d7a968476ed37403f0708
Sanjay Rabidas <rabidassanjay@gmail.com>
2016-06-16 21:35:11 +0530

    video gallery listing

--- application/views/videos/index.php
+++ application/views/videos/index.php
@@ -3,6 +3,11 @@
 <head>
   <title>bharatbhutan | Image video</title>
   <?php $this->load->view('include/css_common'); ?>
+  <style>
+    .video-item, .video-details{
+      padding: 15px 0;
+    }
+  </style>
 </head>
 <body class="">
   <?php $this->load->view('include/header'); ?>
@@ -16,31 +21,41 @@
           </div>
 
           <div class="video-grid">
-            <div class="row">
+
               <?php
                 foreach($video_infos as $video_info):
               ?>
-                <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                  <div style="background:#fff;padding:8px;">
-                    <div class="video-item">
-                      <a href="<?php echo base_url('video/view/'. $video_info->video_id); ?>" >
-                      <img width="100%" src="https://img.youtube.com/vi/<?php echo $video_info->youtube_video_id ; ?>/0.jpg">
-                      <!-- <iframe width="600" height="400" src="<?php echo $video_info->video_link ; ?>" frameborder="0" allowfullscreen></iframe> -->
-                    </a>
-                    </div>
-                    <div class="video-caption h5">
-                      <a href="<?php echo base_url('video/view/'. $video_info->video_id); ?>" >
-                        <?php echo $video_info->video_name; ?>
+              <div class="row" style="background:#fff;margin-bottom:15px;">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                  <div class="row">
+                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+                      <div class="video-item">
+                        <a href="<?php echo base_url('video/view/'. $video_info->video_id); ?>" >
+                        <img width="100%" src="https://img.youtube.com/vi/<?php echo $video_info->youtube_video_id ; ?>/0.jpg">
+                        <!-- <iframe width="600" height="400" src="<?php echo $video_info->video_link ; ?>" frameborder="0" allowfullscreen></iframe> -->
                       </a>
+                      </div>
                     </div>
-                    <div class="video-description">
-                      <?php echo $video_info->description; ?>
+                    <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12">
+                      <div class="video-details" style="background:#fff;padding:8px;">
+                      <div class="video-caption h5">
+                        <a href="<?php echo base_url('video/view/'. $video_info->video_id); ?>" >
+                          <?php echo $video_info->video_name; ?>
+                        </a>
+                      </div>
+                      <div class="video-description">
+                        <?php echo $video_info->description; ?>
+                      </div>
+                    </div>
                     </div>
                   </div>
 
+
+
                 </div>
-              <?php endforeach; ?>
-            </div>
+              </div>
+            <hr>
+            <?php endforeach; ?>
           </div>
 
         </div>
@@ -59,5 +74,6 @@
   <?php $this->load->view('include/footer'); ?>
   <?php $this->load->view('include/templates'); ?>
   <?php $this->load->view('include/js_common'); ?>
+
 </body>
 </html>
